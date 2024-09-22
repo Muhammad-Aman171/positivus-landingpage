6af4267cc62b8554971005082e0e2bc038f0220f
Muhammad-Aman171 <ag3734359@gmail.com>
2024-09-22 22:14:15 +0500

    working

--- app/components/card/card.tsx
+++ app/components/card/card.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-import './card.scss'
+import { BsArrowUpRightCircleFill } from "react-icons/bs";
 
-const Card =()=> {
+import Img from "@/public/card/card-img1.png";
+
+import "./card.scss";
+
+const Card = () => {
   return (
-    <div>
-        
+    <div className="card">
+      <div className="card-heading">
+        <div>
+          <p>Search engine</p>
+          <p>optimization</p>
+        </div>
+        <div className="btn">
+          <BsArrowUpRightCircleFill />
+          <p>Learn more</p>
+        </div>
+      </div>
+      <div className="card-img">
+        <Image src={Img} alt="img does not show" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import './page.scss'
+import Card from './components/card/card';
 
 import Logo1 from '@/public/logos/Company logo.png'
 import Logo2 from '@/public/logos/Company logo (1).png'
@@ -12,7 +13,7 @@ import Img1 from '@/public/assets/img1.png'
 const Home: React.FC = () => {
   return (
     <div className=" font-sapce-grotesk">
-      <div className="section1 border border-black flex  justify-between gap-x-20 my-10  ">
+      <section className="section1 border border-black flex  justify-between gap-x-20 my-10  ">
         <div className="border border-black w-1/2 flex flex-col justify-around items-start ">
           <h1 className=" text-6xl font-bold ">Navigating the digital landscape for success</h1>
           <p className=" text-2xl">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
@@ -21,24 +22,29 @@ const Home: React.FC = () => {
         <div className=" w-1/2  ">
           <Image src={Img1} alt="img does not show" className="w-full h-full" />
         </div>
-      </div>
-      <div className="section2 border border-black flex justify-between flex-wrap my-10">
+      </section>
+      <section className="section2 border border-black flex justify-between flex-wrap my-10">
         <Image src={Logo1} alt="logo does not show"/>
         <Image src={Logo2} alt="logo does not show"/>
         <Image src={Logo3} alt="logo does not show"/>
         <Image src={Logo4} alt="logo does not show"/>
         <Image src={Logo5} alt="logo does not show"/>
         <Image src={Logo6} alt="logo does not show"/>
-      </div>
-      <div className="section3">
+      </section>
+      <section className="section3">
         <div className="heading">
           <h1>Services</h1>
           <p>At our digital marketing agency, we offer a range of services to <br/> help businesses grow and succeed online. These services include:</p>
         </div>
         <div className="cards">
-
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
